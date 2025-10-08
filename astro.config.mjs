17f6b5d70760975133c07b409078bda8b7a36395
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2025-10-09 00:02:27 +0300

    static mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ async function getHighlighter() {
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
